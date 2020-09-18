a87fac5b064e75e56e60fe88d1602dfb68e09350
Алекс Артем <sandronator1975@gmail.com>
2020-09-18 14:22:19 +0300

    main

--- app/main.py
+++ app/main.py
@@ -3,7 +3,42 @@ from flask import Flask
 app = Flask(__name__) 
 
 
-import shop_lnu.views  
+  
 #@app.route("/") 
 #def home_view(): 
 #        return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+from datetime import datetime
+from flask import render_template
+from shop_lnu import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
