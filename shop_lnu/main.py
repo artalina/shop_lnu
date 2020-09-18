f06f9563b486141b9d667bef7cb0e2c95097c66d
Алекс Артем <sandronator1975@gmail.com>
2020-09-18 17:43:34 +0300

    main

--- shop_lnu/main.py
+++ shop_lnu/main.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def home_view(): 
         return "<h1>Welcome to Geeks for Geeks SHOP_lnu</h1>"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
