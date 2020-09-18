d4bd44bd540195c938f249bc24e07b98f15f9290
Алекс Артем <sandronator1975@gmail.com>
2020-09-18 15:59:23 +0300

    main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
-from flask import Flask 
+from flask import Flask
+from datetime import datetime
+from flask import render_template
+from shop_lnu import app 
   
 app = Flask(__name__) 
 
 
   
 @app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+#def home_view(): 
+#        return "<h1>Welcome to Geeks for Geeks</h1>"
+
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
 
 
